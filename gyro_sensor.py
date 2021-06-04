8c4343418e8407442814c3e4296c2aa3d9590680
shashwatdh <shashwat123df@gmail.com>
2021-06-05 01:34:57 +0530

    Changes:  Added file_merge.py - merges two files [rough implementation]  Updated gyro_sensor.py

--- gyro_sensor.py
+++ gyro_sensor.py
@@ -4,91 +4,136 @@ import json
 import os
 import csv
 import multiprocessing
+import datetime
 from pre_process_feat_ext import process_feat_ext
+
 """
     Sensor node records values for a defined window size. In our
-    case window size = 1s.
+    case window size = 5s.
     
     We create 2 processes : Master and Slave
     
-    Master node records and sends rec values to slave node for 
+    Master node records and sends recorded values to slave node for 
     preprocessing and feature extraction.
 """
 
-def Master(conn, reqd_cols):
+def Master(conn):
     
+    reqd_cols = config_data["dataset_reqd_cols"]
+    window_size = config_data['window_size']
+    step_size = window_size // 2
+        
+    # read sensor dataset
     dataset = pd.read_csv('gyro_data1.csv')
     X = dataset.iloc[:, reqd_cols].values
+    
     base_TS = X[0,0]
-    window_data = []
-    
-    config = {'y_label' : 'No',
-          'char_encoding': {'one_hot_encoder': [],
-                            'label_encoder':[]
-                            },
-          'missing_data' : {'Numerical' : 'mean',
-                            'Categorical' : 'most_frequent',
-                            'fill_value': None},
-          'test_split': 0.2,
-          'Normalization': None,
-          'feat_ext' : [0,1,2],
-          'data': None}
-    
-    def send2slave(data):
-        config['data'] = np.array(data[:])
-        #print(config['data'])
-        conn.send(config)
+    sensor_data = []
+    window_data = {
+                    "base_Timestamp" : 0,
+                    "data" : []
+                    }
+
+    def send_slave(data):
+        
+        window_data['base_Timestamp'] = base_TS
+        window_data['data'] = np.array(data[:])[:,1:]
+        conn.send(window_data)
         
     for rec in X:
-        # populating values recorded in a time frame
-        if rec[0] < (base_TS + 5):
-            window_data.append(rec[1:]) # removing timestamp
+        
+        # storing values recorded in a time frame into a list
+        if rec[0] < (base_TS + window_size):
+            sensor_data.append(rec[:]) 
+        
         else:
-            """
+            
+            '''
             Record belongs to different window. So first send populated data
             to slave process for pre-processing and feature extraction.
             
             Set base_TS value and clear window_data. Add new entry in 
             window_data.
-            """
+            '''
             
-            if len(window_data):
-                send2slave(window_data[:])
+            if len(sensor_data):
+                send_slave(sensor_data[:])
             
-            base_TS += 5
-            window_data = []
-            window_data.append(rec[1:]) # removing timestamp
+            base_TS += step_size
+            # from cur window remove entries having timestamp < updated base_TS
+            sensor_data = [rec for rec in sensor_data if rec[0] >= base_TS]         
+            sensor_data.append(rec[:])
              
     
-    # Once data is sent close the connection.
+    # Once whole dataset is sent, close the connection.
     conn.send("End")
     conn.close()
+    
   
 def Slave(conn):
 
-    while 1:
-        config_data = conn.recv()
-        if config_data == "End":
+    def fetch_time_stmp():
+        dt_time = datetime.datetime.now()
+        day = dt_time.strftime("%d")
+        month = dt_time.strftime("%m")
+        hrs = dt_time.strftime("%H")
+        mins = dt_time.strftime("%M")
+        time_stmp = day + month + "_" + hrs + mins
+        return time_stmp
+    
+    def file_dump(file, data):
+        with open(file, 'a') as f:
+            w = csv.writer(f)
+            w.writerows(data)
+            
+    extr_feat_buf = []
+    buf_rec_limit = config_data['max_buf_records']
+    file_dump_limit = config_data['max_dump_count']
+    buf_dump_count = 0 # no. of times buf is dumped into file
+    file_name = "gyro_prep_extr_feat"
+    file = "{0}{1}.csv".format(file_name, fetch_time_stmp())
+    
+    while True:
+        
+        window_data = conn.recv()
+        if window_data == "End":
+            # check if there is data yet to be dumped into file
+            if len(extr_feat_buf):
+                # create new file if enough data is dumped into cur file
+                if buf_dump_count == file_dump_limit:
+                    file = "{0}{1}.csv".format(file_name, fetch_time_stmp())
+                    
+                file_dump(file, extr_feat_buf[:])
             break
+        
         else:
-            X = config_data['data']
-            extr_feat = process_feat_ext(config_data)
-            # print extr_feat to file
-            print(extr_feat)
-            if(len(extr_feat)):
-                with open('gyro_prep_extr_feat.csv', 'a') as f:
-                    w = csv.writer(f)
-                    w.writerow(extr_feat)
-        #print("Received the message: {}".format(config_data))
+            extr_feat = process_feat_ext(window_data, config_data)
+            # if buffer is full store data into file
+            if len(extr_feat_buf) == buf_rec_limit:
+                # create new file if enough data is dumped into cur file
+                if buf_dump_count == file_dump_limit:
+                    file = "{0}{1}.csv".format(file_name, fetch_time_stmp())
+                    buf_dump_count = 0
+                
+                file_dump(file, extr_feat_buf[:])
+                extr_feat_buf = []
+                buf_dump_count += 1
+                
+            extr_feat_buf.append(extr_feat)
+                
   
 if __name__ == "__main__":
-   
-    reqd_cols = [0,2,3,4]
+       
+    # read sensor config data
+    with open("./gyro_config.json") as fp:
+        config_data = json.load(fp)
+    fp.close()
+    
     # creating a pipe
     master_conn, slave_conn = multiprocessing.Pipe()
   
     # creating new processes
-    p1 = multiprocessing.Process(target=Master, args=(master_conn, reqd_cols,))
+    p1 = multiprocessing.Process(target=Master, args=(master_conn,))
     p2 = multiprocessing.Process(target=Slave, args=(slave_conn,))
   
     # running processes
